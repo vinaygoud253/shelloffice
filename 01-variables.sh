5a96370e0a0658c846b86a29b30ddb3a0a35ed02
vinaygoud253 <vinay.singeetham123@gmail.com>
2026-01-28 12:33:55 +0530

    hello4

--- 01-variables.sh
+++ 01-variables.sh
@@ -5,3 +5,5 @@ name2=kasthuri
 echo "$name1: Hello $name2 how are you doing "
 echo "$name2: hello $name1 am good "
 echo "lets do the scropting here its very funny lets do it"
+echo "lets check the srever"
+echo "hello ${name1}"
